69560eb0ddd721a42649e2dcbb23dd6d7bee8c20
lerucom <lerucom@gmail.com>
2019-04-25 21:10:19 +0300

    3 React - пробрасываем данные из парент компонента в чайлд с помощью props 5 React - как работать с формами

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Dropdown from "./Dropdown";
+import Header from "./Header";
+import RegistrationForm from "./RegistrationForm";
+
+const menu = [
+    {
+        link: '/articles',
+        label: 'Articles'
+    },
+    {
+        link: '/contacts',
+        label: 'Contacts'
+    },
+    {
+        link: '/posts',
+        label: 'Posts'
+    }
+];
 
 class App extends Component {
-  render() {
-    return (
-        <div>
-          <Dropdown />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div>
+                    <RegistrationForm />
+                </div>
+                <div>
+                    <Header items={menu} />
+                </div>
+                <div>
+                    <Dropdown />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
+
 // import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
