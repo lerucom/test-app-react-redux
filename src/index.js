c5f1224c2c06f97488e88a892a41ae8befc3ac70
lerucom <lerucom@gmail.com>
2019-04-26 20:08:03 +0300

    1 Redux JS - Вступление

--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+function playlist(state = [], action) {
+    console.log(action);
+    if (action.type === 'ADD_TRACK') {
+        return [ // создаем копию данных и добавляем action.payload
+            ...state,
+            action.payload
+        ];
+    }
+    return state;
+}
+
+const store = createStore(playlist); // store - хранилеще всех наших данных в приложении, любые данные мы тут храним
+
+store.subscribe(() => {
+    console.log('subscribe', store.getState());
+});
+
+store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' }); // type - обязательное поле, payload - это данные
+store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Создали новый store для нашего приложения
+// Передали в него функцию, которая получает екшены и меняет store
+// Подписались на изменения store с помощью функции subscribe
+// Выстрелили action с данными
+// Функция playlist меняет данные с store
+// Теперь в callback функции subscribe мы всегда знаем, как выглядит наш стор и можем что-то перерисовать на странице.
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
