f3f1e09528723ed3e3827cc50d5dbdec7d4dfd4c
lerucom <lerucom@gmail.com>
2019-04-26 23:05:53 +0300

    3 Redux JS - connect в react-redux

--- src/App.js
+++ src/App.js
@@ -1,48 +1,75 @@
 import React, {Component} from 'react';
-import Dropdown from "./Dropdown";
-import Header from "./Header";
-import RegistrationForm from "./RegistrationForm";
-import "./App.css";
-
-const menu = [
-    {
-        link: '/articles',
-        label: 'Articles'
-    },
-    {
-        link: '/contacts',
-        label: 'Contacts'
-    },
-    {
-        link: '/posts',
-        label: 'Posts'
-    }
-];
+import {connect} from 'react-redux'; // декоратор
 
 class App extends Component {
-    submit() {
-        console.log('submit btn pushed', this.testInput.value);
-    }
     render() {
+        console.log(this.props.testStore);
         return (
-            <div className="container">
-                <input type="text" placeholder="test" ref={(input) => this.testInput = input}/>
-                <button onClick={this.submit.bind(this)}>Submit</button>
-                <div>
-                    <RegistrationForm />
-                </div>
-                <div>
-                    <Header items={menu} />
-                </div>
-                <div>
-                    <Dropdown />
-                </div>
+            <div>
+                <input type="text" placeholder="input track name"/>
+                <button>Add track</button>
+                <ul>
+                    {this.props.testStore.map((track, index) =>
+                        <li key={index}>{track}</li>
+                    )}
+                </ul>
             </div>
         );
     }
 }
 
-export default App;
+export default connect(
+    state => ({ // map state to props, легко подписываться на store и следить за изменениями
+        testStore: state // state глобальное состояние нашего store
+    }),
+    dispatch => ({}) //
+)(App);
+
+
+// import Dropdown from "./Dropdown";
+// import Header from "./Header";
+// import RegistrationForm from "./RegistrationForm";
+// import "./App.css";
+//
+// const menu = [
+//     {
+//         link: '/articles',
+//         label: 'Articles'
+//     },
+//     {
+//         link: '/contacts',
+//         label: 'Contacts'
+//     },
+//     {
+//         link: '/posts',
+//         label: 'Posts'
+//     }
+// ];
+
+// class App extends Component {
+//     submit() {
+//         console.log('submit btn pushed', this.testInput.value);
+//     }
+//     render() {
+//         return (
+//             <div className="container">
+//                 <input type="text" placeholder="test" ref={(input) => this.testInput = input}/>
+//                 <button onClick={this.submit.bind(this)}>Submit</button>
+//                 <div>
+//                     <RegistrationForm />
+//                 </div>
+//                 <div>
+//                     <Header items={menu} />
+//                 </div>
+//                 <div>
+//                     <Dropdown />
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default App;
 
 // import React from 'react';
 // import logo from './logo.svg';
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
 
-function playlist(state = [], action) {
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = [
+    'Smells like spirit',
+    'Enter Sandman'
+];
+
+function playlist(state = initialState, action) {
     console.log(action);
     if (action.type === 'ADD_TRACK') {
         return [ // создаем копию данных и добавляем action.payload
@@ -17,31 +24,55 @@ function playlist(state = [], action) {
 }
 
 const store = createStore(playlist); // store - хранилеще всех наших данных в приложении, любые данные мы тут храним
-const addTrackBtn = document.querySelector('.addTrack');
-const list = document.querySelector('.list');
-const trackInput = document.querySelector('.trackInput');
 
-store.subscribe(() => {
-    console.log('subscribe', store.getState());
-    list.innerHTML = '';
-    trackInput.value = '';
-    store.getState().forEach(track => {
-        const li = document.createElement('li');
-        li.textContent = track;
-        list.appendChild(li);
-    })
-});
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
-store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' }); // type - обязательное поле, payload - это данные
-store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
+serviceWorker.unregister();
 
-addTrackBtn.addEventListener('click', () => {
-    const trackName = trackInput.value;
-    console.log('trackName, trackName');
-    store.dispatch({ type: 'ADD_TRACK', payload: trackName });
-});
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// function playlist(state = [], action) {
+//     console.log(action);
+//     if (action.type === 'ADD_TRACK') {
+//         return [ // создаем копию данных и добавляем action.payload
+//             ...state,
+//             action.payload
+//         ];
+//     }
+//     return state;
+// }
+//
+// const store = createStore(playlist); // store - хранилеще всех наших данных в приложении, любые данные мы тут храним
+
+// const addTrackBtn = document.querySelector('.addTrack');
+// const list = document.querySelector('.list');
+// const trackInput = document.querySelector('.trackInput');
+//
+// store.subscribe(() => {
+//     console.log('subscribe', store.getState());
+//     list.innerHTML = '';
+//     trackInput.value = '';
+//     store.getState().forEach(track => {
+//         const li = document.createElement('li');
+//         li.textContent = track;
+//         list.appendChild(li);
+//     })
+// });
+//
+// store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' }); // type - обязательное поле, payload - это данные
+// store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
+//
+// addTrackBtn.addEventListener('click', () => {
+//     const trackName = trackInput.value;
+//     console.log('trackName, trackName');
+//     store.dispatch({ type: 'ADD_TRACK', payload: trackName });
+// });
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
 
 // Создали новый store для нашего приложения
 // Передали в него функцию, которая получает екшены и меняет store
@@ -49,8 +80,9 @@ ReactDOM.render(<App/>, document.getElementById('root'));
 // Выстрелили action с данными
 // Функция playlist меняет данные с store
 // Теперь в callback функции subscribe мы всегда знаем, как выглядит наш стор и можем что-то перерисовать на странице.
-
+//
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// serviceWorker.unregister();
