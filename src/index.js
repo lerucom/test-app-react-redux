80342cc2a6e7f98759d6369f5b95ab852daec87a
lerucom <lerucom@gmail.com>
2019-04-26 21:35:20 +0300

    2 Redux JS - отрисовываем елементы из store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import {createStore} from 'redux';
 
 function playlist(state = [], action) {
     console.log(action);
@@ -17,15 +17,31 @@ function playlist(state = [], action) {
 }
 
 const store = createStore(playlist); // store - хранилеще всех наших данных в приложении, любые данные мы тут храним
+const addTrackBtn = document.querySelector('.addTrack');
+const list = document.querySelector('.list');
+const trackInput = document.querySelector('.trackInput');
 
 store.subscribe(() => {
     console.log('subscribe', store.getState());
+    list.innerHTML = '';
+    trackInput.value = '';
+    store.getState().forEach(track => {
+        const li = document.createElement('li');
+        li.textContent = track;
+        list.appendChild(li);
+    })
 });
 
 store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' }); // type - обязательное поле, payload - это данные
 store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+addTrackBtn.addEventListener('click', () => {
+    const trackName = trackInput.value;
+    console.log('trackName, trackName');
+    store.dispatch({ type: 'ADD_TRACK', payload: trackName });
+});
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // Создали новый store для нашего приложения
 // Передали в него функцию, которая получает екшены и меняет store
